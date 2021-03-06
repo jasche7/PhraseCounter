214c5b086560ccfa92d7ae95b743eac9d58cd211
Jasche7 <jasonc1172@gmail.com>
2021-03-05 17:38:31 -0800

    Added logger and updated comments

---
+++ src/main/java/com/jasche/phrasecounter/MyLogger.java
@@ -0,0 +1,9 @@
+package com.jasche.phrasecounter;
+
+import java.util.logging.Logger;
+
+public class MyLogger {
+    public static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+    private MyLogger(){}
+}
