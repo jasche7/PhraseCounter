2486ce2f83767b92b75c4587d4779437ff9c0d32
Jasche7 <jasonc1172@gmail.com>
2021-03-09 16:18:51 -0800

    Added PrintMap; now fully functional

--- src/main/java/com/jasche/phrasecounter/PhraseCounter.java
+++ src/main/java/com/jasche/phrasecounter/PhraseCounter.java
@@ -21,13 +21,14 @@ public class PhraseCounter {
             WordMapper map = new WordMapper(wordsList);
             Map<String, Integer> wordsMap = map.mapWordCount();
             Map<String, Integer> sortedWordsMap = MapUtil.sortByValue(wordsMap);
+            PrintMap.printSortedMap(sortedWordsMap);
 
             String logWords = "Words: " + wordsList.toString();
             String logMap = "Map: " + wordsMap.entrySet().toString();
             String logSortedMap = "Sorted Map: " + sortedWordsMap.entrySet().toString();
-            LOGGER.info(logWords);
-            LOGGER.info(logMap);
-            LOGGER.info(logSortedMap);
+            LOGGER.fine(logWords);
+            LOGGER.fine(logMap);
+            LOGGER.fine(logSortedMap);
         } catch (FileNotFoundException e){
             LOGGER.severe("File not found.");
         }
