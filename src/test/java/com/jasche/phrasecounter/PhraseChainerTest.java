e22ad5c72657c79a75ad7629b5a4fa4cd409961a
Jasche7 <jasonc1172@gmail.com>
2021-03-12 23:35:50 -0800

    Added tests for maxPhraseLength

--- src/test/java/com/jasche/phrasecounter/PhraseChainerTest.java
+++ src/test/java/com/jasche/phrasecounter/PhraseChainerTest.java
@@ -11,6 +11,14 @@ class PhraseChainerTest {
 
     private static List<String> testList;
     private static List<String> correctList;
+    private static List<String> correctListForLength1;
+    private static List<String> correctListForLength2;
+
+    List<String> generateTestPhrases(int maxPhraseLength){
+        List<String> testPhrases = new LinkedList<>(PhraseChainer.chainWords(testList, maxPhraseLength));
+        testPhrases.sort(Comparator.naturalOrder());
+        return testPhrases;
+    }
 
     @BeforeAll
     static void init(){
@@ -43,13 +51,47 @@ class PhraseChainerTest {
                 "first",
                 "first napkin",
                 "napkin"));
+        correctListForLength1 = new LinkedList<>(testList);
+        correctListForLength2 = new LinkedList<>(List.of(
+                "I",
+                "I have",
+                "have",
+                "have taken",
+                "taken",
+                "taken the",
+                "the",
+                "the first",
+                "first",
+                "first napkin",
+                "napkin"
+        ));
+
         correctList.sort(Comparator.naturalOrder());
+        correctListForLength1.sort(Comparator.naturalOrder());
+        correctListForLength2.sort(Comparator.naturalOrder());
     }
 
     @Test
-    void canBuildPhrase() {
-        List<String> testPhrases = new LinkedList<>(PhraseChainer.chainWords(testList, 0));
-        testPhrases.sort(Comparator.naturalOrder());
+    void canBuildPhrase(){
+        List<String> testPhrases = generateTestPhrases(0);
+        assertEquals(correctList, testPhrases);
+    }
+
+    @Test
+    void canSetMaxPhraseLength1(){
+        List<String> testPhrases = generateTestPhrases(1);
+        assertEquals(correctListForLength1, testPhrases);
+    }
+
+    @Test
+    void canSetMaxPhraseLength2(){
+        List<String> testPhrases = generateTestPhrases(2);
+        assertEquals(correctListForLength2, testPhrases);
+    }
+
+    @Test
+    void canSetMaxPhraseLengthHigh(){
+        List<String> testPhrases = generateTestPhrases(100);
         assertEquals(correctList, testPhrases);
     }
 }
